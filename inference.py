4cc3fee3529c79194e8470008194aafb4cbfb5b2
Anh Phan <anhphanvt2705@gmail.com>
2024-07-02 15:44:29 +0700

    inference without label tags

--- inference.py
+++ inference.py
@@ -3,4 +3,4 @@ from mmseg.apis import MMSegInferencer
 # Load models into memory
 inferencer = MMSegInferencer(model='pidnet-s_2xb6-120k_1024x1024-cityscapes', device='cuda:0')
 # Inference
-inferencer('image.png', show=True)
+inferencer('image.png', show=True, with_labels=False)
