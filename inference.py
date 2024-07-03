cdb1413e6d20be38e4a2cf8f74ecaad120a4167d
Anh Phan <anhphanvt2705@gmail.com>
2024-07-03 09:44:28 +0700

    update inference & output sample

--- inference.py
+++ inference.py
@@ -1,6 +1,11 @@
-from mmseg.apis import MMSegInferencer
+from mmseg.apis import init_model, inference_model, show_result_pyplot
 
-# Load models into memory
-inferencer = MMSegInferencer(model='pidnet-s_2xb6-120k_1024x1024-cityscapes', device='cuda:0')
-# Inference
-inferencer('image.png', show=True, with_labels=False)
+config_path = 'configs\pidnet\pidnet-s_2xb6-120k_1024x1024-cityscapes.py'
+checkpoint_path = 'checkpoints\pidnet-s_9591acc_7691iou.pth'
+img_path = 'samples/image.png'
+
+
+model = init_model(config_path, checkpoint_path)
+result = inference_model(model, img_path)
+
+vis_iamge = show_result_pyplot(model, img_path, result,with_labels=False, out_file='samples/output/result.png')
